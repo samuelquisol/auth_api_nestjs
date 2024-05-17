67bc5368b0dde61595c41d578437ab5f10382239
Samuel Quintero Solís <samuelquisol@gmail.com>
2024-05-16 19:39:03 -0500

    Initial Persistence Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { UsersModule } from './users/users.module';
+import dbConfig from './libs/persistence/db-config';
+import { PersistenceModule } from './libs/persistence';
+import { AtGuard } from './libs/auth/guards/at.guard';
+import { AuthModule } from '@libs/auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [dbConfig],
+      isGlobal: true,
+    }),
+    PersistenceModule,
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/libs/auth/guards/at.guard.ts
@@ -0,0 +1,47 @@
+import {
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { Logger } from '@nestjs/common';
+
+@Injectable()
+export class AtGuard extends AuthGuard('jwt') {
+  private readonly logger = new Logger(AtGuard.name);
+
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  // canActivate: This method is called before the route handler. It determines whether the route can be accessed. It uses the Reflector to check if the 'IsPublic' metadata is set for the route handler or the controller class. If 'IsPublic' is true, the route can be accessed without authentication.
+  canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride('IsPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) return Promise.resolve(true);
+
+    return super.canActivate(context) as Promise<boolean>;
+  }
+
+  // handleRequest: This method is called after the JWT authentication is done. If there's an error or info message (usually when the JWT is expired or invalid), it logs the error and throws an HttpException with a 401 Unauthorized status. If there's no user (which means the JWT didn't contain valid user information), it logs a warning and throws an UnauthorizedException. If everything is fine, it simply returns the user.
+
+  handleRequest(err, user, info: Error) {
+    if (err || info) {
+      this.logger.error(`JWT error: ${info.message || err}`);
+      throw new HttpException('Token is expired!', HttpStatus.UNAUTHORIZED);
+    }
+
+    if (!user) {
+      this.logger.warn('Access Denied: Unauthorized access attempt');
+      throw new UnauthorizedException('Access Denied.');
+    }
+
+    return user;
+  }
+}
